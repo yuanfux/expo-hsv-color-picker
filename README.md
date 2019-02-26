b0673ffe6eb707dd9c595af80084b50b085cae4a
Yuan Fu <fuyuanx@gmail.com>
2019-02-26 11:58:11 +0800

    done

---
+++ README.md
@@ -0,0 +1,4 @@
+# expo-hsv-color-picker
+
+> an Expo HSV(hue, saturation, value) color picker
+> This is the Expo compatible version of [`react-native-hsv-color-picker`](https://github.com/yuanfux/react-native-hsv-color-picker)
---
+++ demo/metro.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const blacklist = require('metro-config/src/defaults/blacklist');
+const escapeRegexString = require('escape-regex-string');
+
+module.exports = {
+  resolver: {
+    blacklistRE: blacklist([
+      new RegExp(
+        `^${escapeRegexString(path.resolve(__dirname, '..', 'node_modules'))}\\/.*$`,
+      ),
+    ]),
+    providesModuleNodeModules: [
+      'react-native',
+      'react',
+      'prop-types',
+    ],
+    extraNodeModules: {
+      '@babel/runtime': path.resolve(__dirname, 'node_modules/@babel/runtime'),
+      'expo': path.resolve(__dirname, 'node_modules/expo'),
+      'chroma-js': path.resolve(__dirname, 'node_modules/chroma-js')
+    },
+  },
+  projectRoot: path.resolve(__dirname),
+  watchFolders: [
+    path.resolve(__dirname, '..'),
+  ],
+};
